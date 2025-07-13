fbb28195f46af6682c49e87fb7b4b953a0a94b11
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2025-07-13 21:55:37 +0200

    initial commit project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# e-commerce-produc-page-luc
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path';
+
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  root:'.',
+  build:{
+    outDir:"dist",
+    emptyOutDir: true,
+    rollupOptions:{
+      input: {main: resolve(__dirname, 'index.html'),
+      product: resolve(__dirname, 'product.html'),
+  }
+}
+},
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@stores': resolve(__dirname, 'src/stores'),
+      '@data': resolve(__dirname, 'src/data'),
+      '@types': resolve(__dirname, 'src/types'),
+    }
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
